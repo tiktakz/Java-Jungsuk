b2d1aca9f5d182bed0242cfb2d525fddfce63bba
tiktakz <whitehouseon@naver.com>
2022-10-26 22:14:10 +0900

    메소드 오버라이딩

--- JavaStudies/src/Inheritance/CustomerTest1.java
+++ JavaStudies/src/Inheritance/CustomerTest1.java
@@ -3,12 +3,28 @@ package Inheritance;
 public class CustomerTest1 {
     public static void main(String[] args) {
         Customer customerLee = new Customer(10100,"LEE");
-
-        Customer customerKim = new VIPCustomer(10101,"Kim");
+        Customer customerKim = new VIPCustomer(10101,"Kim", 50000);
         // 하위 클래스 생성시 상위클래스가 먼저 호출됨.
         customerKim.bonusPoint = 1000;
 
         System.out.println(customerLee.showCustomerInfo());
         System.out.println(customerKim.showCustomerInfo());
+
+        // 오버라이딩 테스트
+        System.out.println("--------");
+        Customer customer1 = new Customer(30000, "Test Lee");
+        customer1.calcPrice(10000);
+        System.out.println(customer1.showCustomerInfo());
+
+        // VIPCustomer calcPrice() overide
+        VIPCustomer customer2 = new VIPCustomer(40000, "Test Kim",100);
+        customer2.calcPrice(10000);
+        System.out.println(customer2.showCustomerInfo());        
+
+        Customer customerWho = new VIPCustomer(100010, "WHO", 200);
+        int price = customerWho.calcPrice(10000);
+        System.out.println(customerWho.showCustomerInfo());
+        //500
+        // Customer타입 이지만 VIPCustomer으로 생성되며, 그 클래스의 메소드가 호출됨
     }
 }
--- JavaStudies/src/Inheritance/VIPCustomer.java
+++ JavaStudies/src/Inheritance/VIPCustomer.java
@@ -5,12 +5,19 @@ public class VIPCustomer extends Customer {
     private int agentID;
     private double saleRatio;
 
-    public VIPCustomer(int customerID, String customerName) {
+    public VIPCustomer(int customerID, String customerName, int agentID) {
         super(customerID, customerName);
         customerGrade = "VIP";
         bonusRatio = 0.05;
         saleRatio = 0.1;
+        this.agentID = agentID;
     }
     
     public int getAgentID() {return agentID;}
+
+    public int calcPrice(int price) {
+        // Override method
+        bonusPoint += price * bonusRatio;
+        return price - (int)(price*saleRatio);
+    }
 }
